5c8b174f3681ef868a8a9bbec1134e6a124bd6a2
Sasitorn Ph. <sasitorn.9439@gmail.com>
2026-06-25 20:56:40 +0700

    Update claude.js

--- api/claude.js
+++ api/claude.js
@@ -2,11 +2,16 @@ export default async function handler(req, res) {
 
   const apiKey = process.env.GEMINI_API_KEY;
 
-  const prompt =
-    req.body.prompt ||
-    req.body.message ||
-    "";
+const prompt =
+  req.body?.prompt ||
+  req.body?.message ||
+  "";
 
+if (!prompt) {
+  return res.status(400).json({
+    error: "Prompt is empty"
+  });
+}
   const response = await fetch(
     `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?key=${apiKey}`,
     {
